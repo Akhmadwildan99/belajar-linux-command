f316bb116b7661c1c3410ed552b74a4d61a23464
wildan <wildanakhmad3584@gmail.com>
2024-06-02 19:39:49 +0700

    init

---
+++ command.sh
@@ -0,0 +1,22 @@
+### if you do not know where you are
+pwd
+
+### to make a directory
+mkdir foo
+
+### make a directory with multiple nested folder
+mkdir foo/bar
+
+### to make multiple folder
+mkdir foo bar
+
+### delete folder
+rmdir foo
+
+### delete multiple folder
+rmdir foo bar
+
+### but rmdir can be use if the folder is empty
+### we can use rm command with options -rf
+rm -rf foo bar
+
